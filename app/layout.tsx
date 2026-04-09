f93590448bb66b4efdb6094a5952842612afd9d0
dahmoud <dahmoudjerrah@gmail.com>
2026-04-10 01:02:18 +0200

    udate logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,10 @@ import { AuthProvider } from "../components/AuthProvider";
 export const metadata: Metadata = {
   title: "  أهل الطالب محمد ",
   description: "نظام إدارة صندوق التكافل والتضامن",
+   icons: {
+    icon: "/talebmed.svg",
+    apple: "/talebmed.svg",
+  },
 };
 
 export default function RootLayout({
